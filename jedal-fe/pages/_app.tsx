9e60bda2f236d8d330f5bd8912227ca43ec0d041
Jaemin Ahn <jae7415@gmail.com>
2020-04-10 23:15:50 +0900

    fea) 코드 컨벤션 설정

--- jedal-fe/pages/_app.tsx
+++ jedal-fe/pages/_app.tsx
@@ -1,52 +1,53 @@
-import React from "react"
-import App from "next/app"
-import Head from "next/head"
-import { ThemeProvider, createGlobalStyle } from "styled-components"
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
 import withApollo from '../hooks/withApollo';
-import {ApolloClient, NormalizedCacheObject} from 'apollo-boost';
+import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
 
 export interface ITheme {
-  niceBlack: string;
+    niceBlack: string;
 }
 
 export interface IThemeWrapper {
-  theme: ITheme;
+    theme: ITheme;
 }
 
 export const theme: ITheme = {
-  niceBlack: "#001F3F",
-}
+    niceBlack: '#001F3F',
+};
 
 const GlobalStyle = createGlobalStyle<IThemeWrapper>`
   body {
     margin: 0 auto;
-    color: ${props => props.theme.niceBlack}; 
+    color: ${(props) => props.theme.niceBlack}; 
   }
-`
+`;
 interface IPops {
-  apollo: ApolloClient<NormalizedCacheObject>;
+    apollo: ApolloClient<NormalizedCacheObject>;
 }
 
 class MyApp extends App<IPops> {
-  render() {
-    const { Component, pageProps, apollo } = this.props
+    render() {
+        const { Component, pageProps, apollo } = this.props;
 
-    return (
-      <React.Fragment>
-        <Head>
-          <title>GraphQL Job Board</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
-        <ApolloProvider client={apollo}>
-          <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </ApolloProvider>
-      </React.Fragment>
-    )
-  }
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>GraphQL Job Board</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </Head>
+                <ApolloProvider client={apollo}>
+                    <ThemeProvider theme={theme}>
+                        <GlobalStyle />
+                        <Component {...pageProps} />
+                    </ThemeProvider>
+                </ApolloProvider>
+            </React.Fragment>
+        );
+    }
 }
 
 export default withApollo(MyApp);
--- jedal-fe/pages/index.tsx
+++ jedal-fe/pages/index.tsx
@@ -5,28 +5,28 @@ import { useQuery } from '@apollo/react-hooks';
 import JobList from '../components/JobList';
 
 const Index = () => {
-  const JOBS_QUERY = gql`
-    query {
-      jobs {
-        id
-        title
-        applyUrl
-        company {
-          name
+    const JOBS_QUERY = gql`
+        query {
+            jobs {
+                id
+                title
+                applyUrl
+                company {
+                    name
+                }
+            }
         }
-      }
-    }
-  `;
+    `;
 
-  const jobs = useQuery(JOBS_QUERY);
-  
-  return (
-  <div>
-    <h1>GraphQL Job Board</h1>
-    <p>A list of open GraphQL jobs.</p>
-    <JobList jobs={jobs?.data?.jobs || []}/>
-  </div>
-  )
-}
+    const jobs = useQuery(JOBS_QUERY);
+
+    return (
+        <div>
+            <h1>GraphQL Job Board</h1>
+            <p>A list of open GraphQL jobs.</p>
+            <JobList jobs={jobs?.data?.jobs || []} />
+        </div>
+    );
+};
 
 export default Index;
