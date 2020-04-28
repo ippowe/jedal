0614a96ffb5833a848f93fe4a21f51a6a1f15be4
Jaemin Ahn <jae7415@gmail.com>
2020-04-28 16:19:42 +0900

    사용하지 않는 ThemeProvider 제거

--- jedal-fe/pages/_app.tsx
+++ jedal-fe/pages/_app.tsx
@@ -2,29 +2,10 @@
 import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
 import { ApolloProvider } from '@apollo/react-hooks';
 import withApollo from '../hooks/withApollo';
 import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
 
-export interface ITheme {
-    niceBlack: string;
-}
-
-export interface IThemeWrapper {
-    theme: ITheme;
-}
-
-export const theme: ITheme = {
-    niceBlack: '#001F3F',
-};
-
-const GlobalStyle = createGlobalStyle<IThemeWrapper>`
-  body {
-    margin: 0 auto;
-    color: ${(props) => props.theme.niceBlack}; 
-  }
-`;
 interface IPops {
     apollo: ApolloClient<NormalizedCacheObject>;
 }
@@ -40,10 +21,7 @@ class MyApp extends App<IPops> {
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </Head>
                 <ApolloProvider client={apollo}>
-                    <ThemeProvider theme={theme}>
-                        <GlobalStyle />
-                        <Component {...pageProps} />
-                    </ThemeProvider>
+                    <Component {...pageProps} />
                 </ApolloProvider>
             </React.Fragment>
         );
