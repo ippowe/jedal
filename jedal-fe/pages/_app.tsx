ecce8fefcbf1c652fa5a718451b2e6068ea37002
Jaemin Ahn <jae7415@gmail.com>
2020-04-28 16:24:00 +0900

    NomalizeCSS 적용

--- jedal-fe/pages/_app.tsx
+++ jedal-fe/pages/_app.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import App from 'next/app';
 import Head from 'next/head';
 import { ApolloProvider } from '@apollo/react-hooks';
-import withApollo from '../hooks/withApollo';
 import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
+
+import withApollo from '../hooks/withApollo';
+import NomalizeCSS from '../styles/nomalizeCSS';
 
 interface IPops {
     apollo: ApolloClient<NormalizedCacheObject>;
@@ -21,6 +23,7 @@ class MyApp extends App<IPops> {
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </Head>
                 <ApolloProvider client={apollo}>
+                    <NomalizeCSS />
                     <Component {...pageProps} />
                 </ApolloProvider>
             </React.Fragment>
