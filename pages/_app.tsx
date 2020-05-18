ae873f6060a11ba3195cbf1fe91216431d6ff2af
Jaemin Ahn <jae7415@gmail.com>
2020-05-18 22:47:47 +0900

    fea)삭제된 Redux Provider 복원

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient, NormalizedCacheObject } from 'apollo-boost';
-
-import withApollo from '../hooks/withApollo';
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../modules";
+import withApollo from "../hooks/withApollo";
 import NormalizeCSS from '../styles/normalizeCSS';
 
 interface IPops {
   apollo: ApolloClient<NormalizedCacheObject>;
 }
+
+const store = configureStore(
+  { reducer: rootReducer }
+);
 
 class MyApp extends App<IPops> {
   render() {
@@ -22,10 +28,12 @@ class MyApp extends App<IPops> {
           <title>GraphQL Job Board</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <ApolloProvider client={apollo}>
-          <NormalizeCSS />
-          <Component {...pageProps} />
-        </ApolloProvider>
+        <Provider store={store}>
+          <ApolloProvider client={apollo}>
+            <NormalizeCSS />
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </Provider>
       </React.Fragment>
     );
   }
