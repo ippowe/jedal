369b97ed03dfcd5a446119ad4929d829d0a85d43
yongpilmoon <m71661@gmail.com>
2020-05-27 00:07:58 +0900

    프로세스4 구현

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,11 +7,13 @@ interface Props {
     variant?: Variant;
     children: React.ReactNode;
     buttonCss?: CSSProp;
+    width?: string;
+    size?: 'big';
 }
 
-const Container = styled.button<{ variant: Variant; css: CSSProp }>`
-    width: 245px;
-    height: 40px;
+const Container = styled.button<{ variant: Variant; css: CSSProp; width?: string; size?: 'big' }>`
+    width: ${({ width }) => (width ? width : '100%')};
+    height: ${({ size }) => (size === 'big' ? '48px' : '40px')};
     display: flex;
     align-items: center;
     justify-content: center;
@@ -31,10 +33,12 @@ const Button: React.FC<Props & React.HTMLAttributes<HTMLButtonElement>> = ({
     children,
     variant = 'standard',
     buttonCss,
+    width,
+    size,
     ...otherProps
 }) => {
     return (
-        <Container variant={variant} css={buttonCss} {...otherProps}>
+        <Container variant={variant} css={buttonCss} width={width} size={size} {...otherProps}>
             {children}
         </Container>
     );
